2008f48cc2f2b0ee9ce101a0a42a78913b2b8dc8
jcichon <cichon.kuba@gmail.com>
2021-12-01 21:17:18 +0100

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿
+using NaturalnieApp2.ViewModels.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalnieApp2.ViewModels
+{
+    internal class MainViewModel: ViewModelBase
+    {
+        private ViewModelBase _selectedView = new MainLayoutViewModel();
+
+        public ViewModelBase SelectedView
+        {
+            get { return _selectedView; }
+            set { _selectedView = value; }
+        }
+
+        private ViewModelBase _menuBarView = new MenuBarViewModel();
+
+        public ViewModelBase MenuBarView
+        {
+            get { return _menuBarView; }
+            set { MenuBarView = value; }
+        }
+
+
+    }
+}
---
+++ ViewModels/Menu/MenuBarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalnieApp2.ViewModels.Menu
+{
+    public class MenuBarViewModel: ViewModelBase
+    {
+
+    }
+}
