aeeec21dc7d11b5a3c48fe7cd776ef48a697c91e
JCichon <jcichon@ra.rockwell.com>
2022-04-06 15:55:54 +0200

    Working on the property display

--- Views/Controls/PropertyDisplay.xaml.cs
+++ Views/Controls/PropertyDisplay.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,55 @@ namespace NaturalnieApp2.Views.Controls
         public PropertyDisplay()
         {
             InitializeComponent();
+            SetContentForVisualPresenter(VisualPresenterType);
         }
+
+        #region Dependency properties
+        public static readonly DependencyProperty HeaderTextProperty = DependencyProperty.Register(name: "HeaderText", typeof(string), 
+            typeof(PropertyDisplay), new PropertyMetadata(null));
+
+        public string HeaderText
+        {
+            get => (string)GetValue(HeaderTextProperty);
+            set => SetValue(HeaderTextProperty, value);
+        }
+
+        public static readonly DependencyProperty VisualPresenterTypeProperty = DependencyProperty.Register(name: "VisualPresenterType", typeof(VisualPresenterTypes),
+        typeof(PropertyDisplay), new FrameworkPropertyMetadata(defaultValue: VisualPresenterTypes.Field, 
+            new PropertyChangedCallback(VisualPresenterTypeChangeCallback)));
+
+        private static void VisualPresenterTypeChangeCallback(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            PropertyDisplay? localSource = source as PropertyDisplay;
+            if (localSource == null) return;
+
+            localSource.SetContentForVisualPresenter((VisualPresenterTypes)e.NewValue);
+        }
+
+        public VisualPresenterTypes VisualPresenterType
+        {
+            get => (VisualPresenterTypes)GetValue(VisualPresenterTypeProperty);
+            set => SetValue(VisualPresenterTypeProperty, value);
+
+        }
+        #endregion
+
+
+        #region Private methods
+        private void SetContentForVisualPresenter(VisualPresenterTypes type)
+        {
+            if (type == VisualPresenterTypes.Field)
+            {
+                FrameworkElement textBlock = new TextBlock();
+                ContentForVisualPresenter.Content = textBlock.Loa;
+            }
+        }
+        #endregion
+    }
+
+    public enum VisualPresenterTypes
+    {
+        Field,
+        List
     }
 }
